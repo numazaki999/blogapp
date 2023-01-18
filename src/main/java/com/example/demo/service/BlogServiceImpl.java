88b01f047eeca133d612948931ff3d58a5f09502
numazaki999 <numazaki26@gmail.com>
2023-01-18 22:58:12 +0900

    [add]BlogServiceImpl.javaの作成

--- src/main/java/com/example/demo/service/BlogServiceImpl.java
+++ src/main/java/com/example/demo/service/BlogServiceImpl.java
@@ -1,5 +1,42 @@
 package com.example.demo.service;
 
-public class BlogServiceImpl {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.Blog;
+import com.example.demo.repository.BlogRepository;
+
+@Service
+public class BlogServiceImpl implements BlogService {
+
+	@Autowired
+	BlogRepository blogRepository;
+	
+	@Override
+	public void saveBlog(Blog blog) {
+		blogRepository.saveOne(blog);
+	}
+	
+	@Override
+	public Optional<Blog> getBlog(Integer number) {
+		return blogRepository.selectOne(number);
+	}
+	
+	@Override
+	public List<Blog> getBlogs() {
+		return blogRepository.selectAll();
+	}
+
+	@Override
+	public void updateBlog(Blog blog) {
+		blogRepository.updateOne(blog);
+	}
+	
+	@Override
+	public void deleteBlog(Integer number) {
+		blogRepository.deleteOne(number);
+	}
 }
