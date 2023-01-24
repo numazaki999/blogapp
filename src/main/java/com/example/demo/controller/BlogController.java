70cab45fa0b0e8b2a6603df7d08991a066f495d2
numazaki999 <numazaki26@gmail.com>
2023-01-24 18:37:41 +0900

    [add]Read機能の追加

--- src/main/java/com/example/demo/controller/BlogController.java
+++ src/main/java/com/example/demo/controller/BlogController.java
@@ -1,9 +1,14 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.entity.Blog;
 import com.example.demo.service.BlogService;
 
 @Controller
@@ -12,5 +17,12 @@ public class BlogController {
 
 	@Autowired
 	BlogService blogServiceImpl;
-
+	
+	//一覧画面表示
+	@GetMapping("/")
+	public String summary(Model model) {
+		List<Blog> blogs = blogServiceImpl.getBlogs();
+		model.addAttribute("blogs", blogs);
+		return "summary";	
+	}
 }
